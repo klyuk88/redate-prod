a4d02708372514114b6197f17eea3bf414cf959f
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2022-06-27 14:55:59 +0300

    prod

---
+++ js/chunk-5e373f30.c7e0285a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-5e373f30"],{"3fd1":function(c,e,n){"use strict";n.r(e);var t=n("7a23"),o=(n("7462"),n("928c"),n("c888"),n("cb59")),u={class:"content__container"},i={setup:function(c){return function(c,e){return Object(t["openBlock"])(),Object(t["createElementBlock"])("div",u,[Object(t["createVNode"])(o["a"])])}}};n("f64b");const r=i;e["default"]=r},e909:function(c,e,n){},f64b:function(c,e,n){"use strict";n("e909")}}]);
+//# sourceMappingURL=chunk-5e373f30.c7e0285a.js.map
